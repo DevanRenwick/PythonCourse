da1f28c4ffd7f08ac2c0853b70358c7f7cf5bb18
DevanRenwick <devanrenwick4@gmail.com>
2023-12-08 18:48:36 -0400

    Started another question

--- Section 6 - Methods and Functions/Coding Exercises/Function Practice Exercises.py
+++ Section 6 - Methods and Functions/Coding Exercises/Function Practice Exercises.py
@@ -155,7 +155,8 @@ print("Part 2, Question 2: ")
 print(paper_doll('Hello'))
 print(paper_doll('Mississippi'))
 
-# BLACKJACK: Given three integers between 1 and 11, if their sum is less than or equal to 21, return their sum. If their sum exceeds 21 and there's an eleven, reduce the total sum by 10. Finally, if the sum (even after adjustment) exceeds 21, return 'BUST'
+# BLACKJACK: Given three integers between 1 and 11, if their sum is less than or equal to 21, return their sum. If their sum exceeds 21 and there's an eleven,
+# reduce the total sum by 10. Finally, if the sum (even after adjustment) exceeds 21, return 'BUST'
 # blackjack(5,6,7) --> 18
 # blackjack(9,9,9) --> 'BUST'
 # blackjack(9,9,11) --> 19
@@ -193,3 +194,23 @@ print("Part 2: Question 3")
 print(blackjack(5,6,7))
 print(blackjack(9,9,9))
 print(blackjack(9,9,11))
+
+
+# SUMMER OF ('69: Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 9 (every 6 will be followed by at least one 9). '
+#            'Return 0 for no numbers.)
+# summer_69([1, 3, 5]) --> 9
+# summer_69([4, 5, 6, 7, 8, 9]) --> 9
+# summer_69([2, 1, 6, 9, 11]) --> 14
+
+def summer_69(numbers):
+    sumOfnumbers = []
+    for i in numbers:
+        if numbers[i] != 6:
+            continue
+        elif numbers[i] == 6:
+
+
+print("Part 2: Final Question ")
+print(summer_69([1, 3, 5]))
+print(summer_69([4, 5, 6, 7, 8, 9]))
+print(summer_69([2, 1, 6, 9, 11]))
