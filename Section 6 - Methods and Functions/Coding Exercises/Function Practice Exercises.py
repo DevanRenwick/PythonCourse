4a82a742dbd1c04450bff28dbd4949dd3f7f7603
DevanRenwick <devanrenwick4@gmail.com>
2023-12-14 19:28:42 -0400

    Correcting answer to makes_twenty

--- Section 6 - Methods and Functions/Coding Exercises/Function Practice Exercises.py
+++ Section 6 - Methods and Functions/Coding Exercises/Function Practice Exercises.py
@@ -79,6 +79,9 @@ def makes_twenty(n1,n2):
         return True
     else:
         return False
+
+    # you can also do just:
+    # return (n1+n2) == 20 or n1 ==20 or n2 ==20)
 print("Question 3: ")
 print(makes_twenty(20,10))
 print(makes_twenty(9,3))
