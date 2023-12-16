00831316b34d0e120f094b1f2f5575246bd99874
DevanRenwick <devanrenwick4@gmail.com>
2023-12-15 21:02:41 -0400

    Alternative solutions to Old Macdonald

--- Section 6 - Methods and Functions/Coding Exercises/Function Practice Exercises.py
+++ Section 6 - Methods and Functions/Coding Exercises/Function Practice Exercises.py
@@ -126,6 +126,23 @@ print(old_macdonald('macdonald'))
 # >>> " ".join(['Hello','world'])
 # >>> "Hello world"
 
+# There are two ways of doing this one here with either .capitalize() or .upper()
+def old_macdonald(name):
+    first_letter = name[0]
+    inbetween = name[1:3] # slicing I believe
+    fourth_letter = name[3]
+    rest = name[4:]
+
+    return first_letter.upper() + inbetween + fourth_letter.upper() + rest
+
+#or
+
+def old_macdonald(name):
+    first_half = name[:3] # The Mac part
+    second_half = name[3:]
+
+    return first_half.capitalize() + second_half.capitalize()
+
 def master_yoda(text):
     reverseSentence = text.split()
     #print(reverseSentence)
