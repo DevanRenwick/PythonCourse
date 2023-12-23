65277940be80c8bc289db578abd477fa124da9ec
DevanRenwick <devanrenwick4@gmail.com>
2023-12-22 23:30:47 -0400

    Milestone project

---
+++ Section 7 - Milestone Project 1/TicTacToe.py
@@ -0,0 +1,13 @@
+# Here are the requirements:
+#
+# 2 players should be able to play the game (both sitting at the same computer)
+# The board should be printed out every time a player makes a move
+# You should be able to accept input of the player position and then place a symbol on the board
+# Feel free to use Google to help you figure anything out (but don't just Google "Tic Tac Toe in Python" otherwise you won't learn anything!) Keep in mind that this project can take anywhere between several hours to several days.
+#
+# There are 4 Jupyter Notebooks related to this assignment:
+#
+# This Assignment Notebook
+# A "Walkthrough Steps Workbook" Notebook
+# A "Complete Walkthrough Solution" Notebook
+# An "Advanced Solution" Notebook
