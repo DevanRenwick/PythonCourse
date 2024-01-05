f4ea47adbde77a226933515719e5aa615aa65e00
DevanRenwick <devanrenwick4@gmail.com>
2024-01-04 20:16:07 -0400

    First steps

--- Section 7 - Milestone Project 1/TicTacToe.py
+++ Section 7 - Milestone Project 1/TicTacToe.py
@@ -3,7 +3,8 @@
 # 2 players should be able to play the game (both sitting at the same computer)
 # The board should be printed out every time a player makes a move
 # You should be able to accept input of the player position and then place a symbol on the board
-# Feel free to use Google to help you figure anything out (but don't just Google "Tic Tac Toe in Python" otherwise you won't learn anything!) Keep in mind that this project can take anywhere between several hours to several days.
+# Feel free to use Google to help you figure anything out (but don't just Google "Tic Tac Toe in Python" otherwise you won't learn anything!)
+# Keep in mind that this project can take anywhere between several hours to several days.
 #
 # There are 4 Jupyter Notebooks related to this assignment:
 #
@@ -12,13 +13,15 @@
 # A "Complete Walkthrough Solution" Notebook
 # An "Advanced Solution" Notebook
 
-def
-    #place holder. Its christmas! Another placeholder, its boxing day!
-    # also a place holder!
-    # another placeholder
-    # also another placeholder
-    # also another placeholder
-    # final placeholder
-    # New year! Starting project tomorrow !
-    # Actually, tomorrow!
-    # Final placeholder
+def display(row1, row2, row3):
+    # Displays the board for players.
+    print(row1)
+    print(row2)
+    print(row3)
+
+row1 = [' ', ' ', ' ']
+row2 = [' ', ' ', ' ']
+row3 = [' ', ' ', ' ']
+
+print("This is my tic tac toe game")
+display(row1, row2, row3)
